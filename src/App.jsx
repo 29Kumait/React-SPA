be6597dd6bc1100a5728fc02769151179f6561cf
kumait mustafa <kumait@gmail.com>
2024-07-06 17:33:38 +0200

    separate AppRoutes component modify App.jsx accordingly

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
 import { useState } from "react";
 import * as stylex from "@stylexjs/stylex";
 import { RocketIcon } from "@primer/octicons-react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import MainPage from "./Pages/MainPage.jsx";
-import Navbar from "./Components/Navbar.jsx";
-import ProjectPage from "./Pages/ProjectPage.jsx";
-import MorePage from "./Pages/MorePage.jsx";
+import { BrowserRouter as Router } from "react-router-dom";
+import Navbar from "./Components/Navbar";
 import DarkModeToggle from "./Components/DarkModeToggle";
-import { styles, nav, darkModeStyles } from "./stylex.jsx";
-import MuseumPage from "./Pages/MuseumPage.jsx";
-import TheProvider from "./Components/Context.jsx";
-import StarredPage from "./Pages/StarredPage.jsx";
+import { styles, nav, darkModeStyles } from "./stylex";
+import TheProvider from "./Components/Context";
+import AppRoutes from "./Components/AppRoutes";
+import routes from "./routes.jsx";
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
@@ -44,56 +41,8 @@ function App() {
               setDarkMode={setDarkMode}
             />
           </div>
-          <Routes>
-            <Route
-              path="/"
-              element={
-                <div data-testid="main-page">
-                  <MainPage />
-                </div>
-              }
-            />
-            <Route
-              path="/home"
-              element={
-                <div data-testid="main-page">
-                  <MainPage />
-                </div>
-              }
-            />
-            <Route
-              path="/projects"
-              element={
-                <div data-testid="project-page">
-                  <ProjectPage />
-                </div>
-              }
-            />
-            <Route
-              path="/more"
-              element={
-                <div data-testid="more-page">
-                  <MorePage />
-                </div>
-              }
-            />
-            <Route
-              path="/museum"
-              element={
-                <div data-testid="museum-page">
-                  <MuseumPage />
-                </div>
-              }
-            />
-            <Route
-              path="/Starred"
-              element={
-                <div data-testid="starred-page">
-                  <StarredPage />
-                </div>
-              }
-            />
-          </Routes>
+          <AppRoutes routes={routes} />{" "}
+          {/* Use AppRoutes with the routes prop */}
         </div>
       </Router>
     </TheProvider>
@@ -101,3 +50,107 @@ function App() {
 }
 
 export default App;
+
+// import { useState } from "react";
+// import * as stylex from "@stylexjs/stylex";
+// import { RocketIcon } from "@primer/octicons-react";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import MainPage from "./Pages/MainPage.jsx";
+// import Navbar from "./Components/Navbar.jsx";
+// import ProjectPage from "./Pages/ProjectPage.jsx";
+// import MorePage from "./Pages/MorePage.jsx";
+// import DarkModeToggle from "./Components/DarkModeToggle";
+// import { styles, nav, darkModeStyles } from "./stylex.jsx";
+// import MuseumPage from "./Pages/MuseumPage.jsx";
+// import TheProvider from "./Components/Context.jsx";
+// import StarredPage from "./Pages/StarredPage.jsx";
+//
+// function App() {
+//   const [darkMode, setDarkMode] = useState(false);
+//   const [isMenuOpen, setIsMenuOpen] = useState(false);
+//   const [activeLink, setActiveLink] = useState("home");
+//   const currentRootStyle = darkMode
+//     ? { ...styles.root, ...darkModeStyles.darkRoot }
+//     : styles.root;
+//
+//   return (
+//     <TheProvider>
+//       <Router basename="/">
+//         <div {...stylex.props(currentRootStyle)}>
+//           <div {...stylex.props(nav.navbar)}>
+//             <button
+//               data-testid="menu-button"
+//               {...stylex.props(styles.button)}
+//               onClick={() => setIsMenuOpen(!isMenuOpen)}
+//             >
+//               <RocketIcon size={29} />
+//             </button>
+//             <Navbar
+//               data-testid="navbar"
+//               activeLink={activeLink}
+//               setActiveLink={setActiveLink}
+//               isMenuOpen={isMenuOpen}
+//             />
+//             <DarkModeToggle
+//               data-testid="dark-mode-toggle"
+//               darkMode={darkMode}
+//               setDarkMode={setDarkMode}
+//             />
+//           </div>
+//           <Routes>
+//             <Route
+//               path="/"
+//               element={
+//                 <div data-testid="main-page">
+//                   <MainPage />
+//                 </div>
+//               }
+//             />
+//             <Route
+//               path="/home"
+//               element={
+//                 <div data-testid="main-page">
+//                   <MainPage />
+//                 </div>
+//               }
+//             />
+//             <Route
+//               path="/projects"
+//               element={
+//                 <div data-testid="project-page">
+//                   <ProjectPage />
+//                 </div>
+//               }
+//             />
+//             <Route
+//               path="/more"
+//               element={
+//                 <div data-testid="more-page">
+//                   <MorePage />
+//                 </div>
+//               }
+//             />
+//             <Route
+//               path="/museum"
+//               element={
+//                 <div data-testid="museum-page">
+//                   <MuseumPage />
+//                 </div>
+//               }
+//             />
+//             <Route
+//               path="/Starred"
+//               element={
+//                 <div data-testid="starred-page">
+//                   <StarredPage />
+//                 </div>
+//               }
+//             />
+//           </Routes>
+//         </div>
+//       </Router>
+//     </TheProvider>
+//   );
+// }
+//
+// export default App;
--- src/Components/AppRoutes.jsx
+++ src/Components/AppRoutes.jsx
@@ -1,14 +1,23 @@
-// // AppRoutes.jsx
-// import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
+import PropTypes from "prop-types"; // Import PropTypes
 
-// function AppRoutes({ routes }) {
-//     return (
-//         <Routes>
-//             {routes.map((route, index) => (
-//                 <Route key={index} path={route.path} element={route.component} />
-//             ))}
-//         </Routes>
-//     );
-// }
+function AppRoutes({ routes }) {
+  return (
+    <Routes>
+      {routes.map((route, index) => (
+        <Route key={index} path={route.path} element={route.component} />
+      ))}
+    </Routes>
+  );
+}
 
-// export default AppRoutes;
+AppRoutes.propTypes = {
+  routes: PropTypes.arrayOf(
+    PropTypes.shape({
+      path: PropTypes.string.isRequired,
+      component: PropTypes.element.isRequired,
+    }),
+  ).isRequired,
+};
+
+export default AppRoutes;
